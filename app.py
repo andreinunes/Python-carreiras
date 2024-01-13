a1198aa8acb66d8199af9737685ab2d97012a14d
andreinunes <53675654+andreinunes@users.noreply.github.com>
2024-01-13 16:49:48 +0000

    Corrigindo jsonify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import DEBUG
-from flask import Flask, render_template, request, redirect, url_for, jsonfiy
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
@@ -30,6 +30,11 @@ VAGAS = [
 @app.route('/')
 def hello():
   return render_template("home.html", vagas=VAGAS)
+
+
+@app.route('/vagas')
+def lista_vagas():
+  return jsonify(VAGAS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
