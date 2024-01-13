772206b4cec526764b6f44cc7428e5682ffe795f
andreinunes <53675654+andreinunes@users.noreply.github.com>
2024-01-13 16:38:01 +0000

    Adição das vagas, separação de arquivos

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 from re import DEBUG
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonfiy
 
 app = Flask(__name__)
 
+VAGAS = [
+  {'id':1,
+    'titulo': 'Analista de Suporte',
+  'localidade': 'São Paulo',
+  'salario': 'R$ 3.500,00',},
+  {'id':2,
+    'titulo': 'Analista de Dados',
+  'localidade': 'Rio de Janeiro',
+  'salario': 'R$ 1.500,00',},
+  {'id':3,
+    'titulo': 'Cientista de Dados',
+  'localidade': 'Ceará',
+  'salario': 'R$ 4.500,00',},
+  {'id':4,
+    'titulo': 'Desenvolvedor Backend',
+  'localidade': 'Pará',
+  'salario': 'R$ 12.500,00',},
+  {'id':5,
+    'titulo': 'Desenvolvedor Frontend',
+  'localidade': 'Amapá',
+  'salario': 'R$ 321.500,00',}
+  
+]
+
 @app.route('/')
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", vagas=VAGAS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
